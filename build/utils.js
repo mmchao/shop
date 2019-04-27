50a5fbd031c0eaac59e9e404fd64eb8486745c64
mmchao <728270679@qq.com>
2019-04-27 18:32:23 +0800

    Signed-off-by: mmchao <728270679@qq.com>

--- build/utils.js
+++ build/utils.js
@@ -48,7 +48,7 @@ exports.cssLoaders = function (options) {
       return ExtractTextPlugin.extract({
         use: loaders,
         fallback: 'vue-style-loader',
-        // publicPath: '../../' // bootstrap打包路径
+        publicPath: '../../' // bootstrap打包路径
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
